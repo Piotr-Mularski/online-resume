a9c838f2aac357b2d8fa5219da640ba23145403f
Piotr Mularski <piomul6473@gmail.com>
2018-07-16 11:53:58 +0200

    changed to v2

--- gulpfile.js
+++
@@ -1,4 +0,0 @@
-const styles = require('./gulp/tasks/styles'),
-	watch = require('./gulp/tasks/watch'),
-	scripts = require('./gulp/tasks/scripts'),
-	build = require('./gulp/tasks/build');
--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,76 @@
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 const path = require('path');
+const webpack = require('webpack');
 
-module.exports = {
-	entry: {
-		main: "./app/assets/scripts/main.js",
-		vendor: "./app/assets/scripts/vendor.js"
-	},
-	output: {
-		path: path.resolve(__dirname, "./app/temp/assets/scripts"),
-		filename: "[name].js"
-	},
-	module: {
-		loaders: [
-			{
-				loader: "babel-loader",
-				query: {
-					presets: ["es2015"]
-				},
-				test: /\.js$/,
-				exclude: /node_modules/
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+
+const VENDOR_LIBS = ['lazysizes'];
+
+module.exports = (env) => {
+	const isProduction = env === 'production';
+	return {
+		resolve: {
+			alias: {
+				img: path.join(__dirname, 'src/img')
 			}
+		},
+		entry: {
+			bundle: ['babel-polyfill', './src/app.js'],
+			vendor: VENDOR_LIBS
+		},
+		output: {
+			path: path.join(__dirname, 'dist'),
+			filename: '[name].js',
+			publicPath: './'
+		},
+		devServer: {
+			contentBase: path.join(__dirname, 'dist'),
+			historyApiFallback: true,
+			publicPath: '/'
+		},
+		module: {
+			rules: [
+				{
+					test: /\.js$/,
+					exclude: /node_modules/,
+					use: 'babel-loader'
+				},
+				{
+					test: /\.(css|scss)$/,
+					use: ExtractTextPlugin.extract({
+						fallback: 'style-loader',
+						use: 'css-loader!postcss-loader!sass-loader'
+					})
+				},
+				{
+					test: /\.jsx?$/,
+					exclude: /node_modules/,
+					use: 'babel-loader'
+				},
+				{
+					test: /\.(png|woff|woff2|eot|ttf|svg|jpg)$/,
+					exclude: /node_modules/,
+					loader: 'url-loader',
+					query: {
+						limit: 1,
+						name: './assets/images/[name].[ext]'
+					}
+				}
+			]
+		},
+		devtool: isProduction ? 'source-maps' : 'inline-source-map',
+		plugins: [
+			new webpack.optimize.CommonsChunkPlugin({
+				names: ['vendor', 'manifest']
+			}),
+			new CleanWebpackPlugin(['dist']),
+			new HtmlWebpackPlugin({ template: path.join(__dirname, 'src/index.html') }),
+			new ExtractTextPlugin('[name].css'),
+			new CopyWebpackPlugin([{ from: './src/img', to: 'assets/images' }, { from: './src/download', to: './assets/download' }])
 		]
-	} 
-}
+	};
+};
